0c3d525fdb0cce9d3839a3bc59ff5745d82b3ad0
Sanjeev Pathak <pathaksanjeev0496@gmail.com>
2022-10-17 22:15:22 +0530

    Uploading on server

---
+++ todolist_app/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from tkinter.tix import IMMEDIATE
+from django import forms
+from todolist_app.models import TaskList
+
+class  TaskForm(forms.ModelForm):
+    class Meta:
+        model = TaskList
+        fields = ['task','done']
